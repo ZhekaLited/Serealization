ba780317eafb875fea1b5c5366afcd22130d2522
ZhekaLited <94718651+ZhekaLited@users.noreply.github.com>
2022-06-17 19:57:46 +0300

    Update Recurs.java

--- src/main/java/Recurs.java
+++ src/main/java/Recurs.java
@@ -1,28 +1,44 @@
-import java.io.File;
+import java.io.*;
 
 public class Recurs {
+    static FileOutputStream fos;
 
-            private void getAllSubFoldersInPath(File path) {
-                File[] files=path.listFiles();
-                try {
-                    for(File file: files) {
-                        if(file.isDirectory()) {// Проверяет что путь это директория а не файл
-
-                            System.out.println("DIRECTORY:"+file.getCanonicalPath()); // Указывает уникальный путь папки
-                            getAllSubFoldersInPath(file);// Рекурсивно найти файлы
-                        } else {
-                            System.out.println("FILE: "+file.getCanonicalPath());
-                        }
-                    }
-                } catch (Exception e) {
-                    e.printStackTrace();
+    private void getAllSubFoldersInPath(File path) throws IOException {
+        File[] files=path.listFiles();
+        try {
+            for(File file: files) {
+                if(file.isDirectory()) {// Проверяет что путь это директория а не файл
+                    System.out.println("DIRECTORY:"+file.getCanonicalPath()); // Указывает уникальный путь папки
+                    String s = "DIRECTORY:"+file.getCanonicalPath()+"\n";
+                    fos.write(s.getBytes(), 0, s.getBytes().length);
+                    getAllSubFoldersInPath(file);// Рекурсивно найти файлы
+                } else {
+                    System.out.println("FILE: "+file.getCanonicalPath());
+                    String s = "FILE: "+file.getCanonicalPath()+"\n";
+                    fos.write(s.getBytes(), 0, s.getBytes().length);
                 }
             }
-
-
-        public static void main(String[] args) {
-            Recurs fw = new Recurs();
-            fw.getAllSubFoldersInPath(new File("D:\\Новая папка"));
+        } catch (Exception e) {
+            e.printStackTrace();
         }
 
     }
+    public static void main(String[] args) throws IOException {
+        Recurs fw = new Recurs();
+        String text = "Hello world!"; // строка для записи
+        try
+        {
+            fos=new FileOutputStream("D:\\\\гк\\\\Androidi/notes.txt");
+            // перевод строки в байты
+            //byte[] buffer = text.getBytes();
+
+            fw.getAllSubFoldersInPath(new File("D:\\гк\\Androidi"));
+            System.out.println("The file has been written");
+            fos.close();
+        }
+        catch(IOException ex){
+
+            System.out.println(ex.getMessage());
+        }
+    }
+        }
